ce4e58819c90d5e644959f301b9381d8f09433a9
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2025-07-19 19:25:35 +0200

    Added test example

--- src/modules/utils.rs
+++ src/modules/utils.rs
@@ -1,3 +1,13 @@
 pub fn greet(name: &str) -> String {
     format!("Hello {name}!")
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_greet() {
+        assert_eq!(greet("example"), "Hello example!");
+    }
+}
