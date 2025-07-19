6f4b1fbe89041100658e03a75a7da0010cdd085b
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2025-07-19 19:20:17 +0200

    Added modularization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use env_logger;
 use log::info;
+mod modules;
 
 fn main() {
     env_logger::builder().format_timestamp(None).init();
-    info!("Hello, world!");
+    let greet = modules::utils::greet("{{username}}");
+    info!("{greet}");
 }
