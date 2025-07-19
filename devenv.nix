fb68f95f8880cd6f38d8e504fc8472084f9250a6
Muxutruk2 <156070698+Muxutruk2@users.noreply.github.com>
2025-07-19 20:55:20 +0200

    Devenv: Better developer experience

--- devenv.nix
+++ devenv.nix
@@ -5,6 +5,14 @@
 
   packages = [ pkgs.cargo-watch pkgs.bacon ];
 
+  enterShell = ''
+    echo Development Shell for {{crate_name}} initialized.
+    echo Cargo version:
+    cargo --version
+    echo Building project...
+    cargo build
+  '';
+
   languages.rust = {
     enable = true;
     mold.enable = true;
@@ -13,10 +21,32 @@
   processes = { cargo-watch.exec = "cargo-watch -x run"; };
 
   tasks = {
-    "{{crate_name}}:build".exec = "cargo build";
-    "devenv:enterShell".after = [ "{{crate_name}}:build" ];
-  };
+    "{{crate_name}}:build" = {
+      exec = "cargo build";
+      execIfModified = [ "src/**/*.rs" "Cargo.toml" "Cargo.lock" ];
+    };
 
+    "{{crate_name}}:check" = { exec = "cargo check"; };
+
+    "{{crate_name}}:lint" = {
+      exec = ''
+        cargo fmt -- --check
+        cargo clippy -- -D warnings
+      '';
+      execIfModified = [ "src/**/*.rs" ];
+    };
+
+    "{{crate_name}}:test" = {
+      exec = "cargo test";
+      execIfModified = [ "src/**/*.rs" "tests/**/*.rs" ];
+    };
+
+    "{{crate_name}}:run" = {
+      exec = "cargo run";
+      before = [ "rust:build" ];
+    };
+
+  };
   enterTest = ''
     cargo test
   '';
